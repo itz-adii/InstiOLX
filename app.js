1f9ae4b994cd13fa74fa5e647e7ddc5421538e74
“Aditya” <adityajnvt@gmail.com>
2024-06-21 18:08:56 +0530

    New Files

---
+++ app.js
@@ -0,0 +1,19 @@
+let myClassElements = document.querySelectorAll(".myClass");
+
+myClassElements.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.stopPropagation();
+    element.style.borderLeft = "4px solid #f1786b";
+  });
+});
+
+document.addEventListener("click", () => {
+  myClassElements.forEach((element) => {
+    element.style.borderLeft = "";
+  });
+});
+
+document.getElementById("image-up").addEventListener("change", function () {
+  var fileName = this.files.length ? this.files[0].name : "No file chosen";
+  document.querySelector(".file-name").textContent = fileName;
+});
